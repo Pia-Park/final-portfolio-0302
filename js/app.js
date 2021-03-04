9e0e65ee8121cdae9adaa20cc0bf28a860d9af5c
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-03 21:39:23 -0800

    added submit email function

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+
+
+$(document).ready(function() {
+    emailjs.init("user_zbhb7nXYLm2s7HiAUFdOX");
+    $('input[name=submit]').click(function(){
+        let templateParams = {
+            name: $('input[name=name]').val(),
+            email: $('input[name=email]').val(),
+            message: $('textarea[name=message]').val()
+        };
+        emailjs.send('service_ghscycf', 'template_j33huix', templateParams).then(function(response){
+            alert('Succes! You send a message to INAE.');
+        }, function(error){
+            alert('Failed. Try again Please.');
+        });
+    });
+});
