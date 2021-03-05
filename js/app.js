7ef032ce6bc5cf69d0609dfb2ea25e2a076f6909
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-05 00:52:51 -0800

    added like function to art and illust

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+// var adminManager = new ActiveXObject('Microsoft.ApplicationHost.WritableAdminManager');
+// adminManager.CommitPath = "MACHINE/WEBROOT/APPHOST/Default Web Site";
 
+// var httpProtocolSection = adminManager.GetAdminSection("system.webServer/httpProtocol", "MACHINE/WEBROOT/APPHOST/Default Web Site");
+// httpProtocolSection.Properties.Item("allowKeepAlive").Value = true;
+
+// adminManager.CommitChanges();
 
 $(document).ready(function() {
     emailjs.init("user_zbhb7nXYLm2s7HiAUFdOX");
